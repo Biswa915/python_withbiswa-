1b412a0bb76fddbbc5297eeac5288b3e8db7dbe6
Biswajit <bs5473287@gmail.com>
2026-08-10 23:52:58 +0530

    added folder

---
+++ python_guru/if-else_elif_statment.py
@@ -0,0 +1,48 @@
+# age= int(input("Enter your age="))
+# if age >=18:
+#     print("you can vote")
+# else:
+#     print("you cannot vote")
+# chem= int(input("enter your chemistry mark="))
+# physic= int(input("enter your physics mark="))
+# if chem>=50 and physic>=50:
+#     print("you are pass")
+# else:
+#     print("you are fail")
+
+# if_elif_else_statement
+# marks = 82
+# if marks >= 90:
+# print("Grade: A")
+# elif marks >= 75:
+# print("Grade: B")
+# elif marks >= 60:
+# print("Grade: C")
+# elif marks >= 40:
+# print("Grade: D")
+# else:
+# print("Grade: F")
+# 90 above -> A
+# 81 - 90 -> B
+# 71 - 80 -> C
+# 61 - 70 -> D
+# 60 below -> Fail
+# """"""
+
+
+# marks =int(input("enter your marks="))
+# if marks >= 90 and marks <=100:
+#     print("A")
+# elif marks >= 81 and marks<=90:
+#     print("B")
+# elif marks >=71 and marks <=80:
+#     print("C")
+# elif marks >= 61 and marks <= 70:
+#     print("D")
+# elif marks >=0 and marks <=60:
+#     print("Fail")
+# else:
+#     print ("invalid marks")
+
+
+### nested if else statement:-
--- python_guru/questionpractis1.py
+++ python_guru/questionpractis1.py
@@ -35,10 +35,51 @@
 
 # Q4: A student scored marks in 3 subjects. Take all three as input,
 # calculate the total and average, and print both using an f-string.
-sub1=int(input("Enter marks for subject 1: "))
-sub2=int(input("Enter marks for subject 2: "))
-sub3=int(input("Enter marks for subject 3: "))
-total=sub1 + sub2 + sub3
-average=total/3
-print(f"Total mark = {total}")
-print(f"Average mark = {average}")
+# sub1=int(input("Enter marks for subject 1: "))
+# sub2=int(input("Enter marks for subject 2: "))
+# sub3=int(input("Enter marks for subject 3: "))
+# total=sub1 + sub2 + sub3
+# average=total/3
+# print(f"Total mark = {total}")
+# print(f"Average mark = {average}")
+# Q7: Take a number as input. Print whether it is positive, negative, or zero.
+# num = int(input("Enter your number: "))
+# if num >0:
+#     print("positive")
+# elif num <0:
+#     print("negative")
+# else:
+#     print("both are zero")
+
+# Q8: Take two numbers as input. Print the greater of the two. If they are
+# equal, print "Both are equal."
+# num1= int(input("Enter your number="))
+# num2= int(input("Enter your number="))
+
+# if num1 > num2:
+#     print(f"{num1} is geater than{num2}")
+# elif num2>num1:
+#     print(f"{num2} is gater than{num1}")
+# else:
+#     print("Both are equal") 
+
+# Q9: Take a student's marks as input. Print their grade based on this scale:
+# · 90 and above -> A
+# · 75 to 89 -> B
+# . 60 to 74 -> C
+# · 40 to 59 -> D
+# . Below 40 -> F
+mark =int(input("Enter your mark"))
+if mark >=90:
+    print("A")
+elif mark >=75 and mark <=89:
+    print("B")
+elif mark >=60 and mark <=74:
+    print("C")
+elif mark >=40 and mark <=59:
+    print("D")
+elif mark >=40:
+    print("f")  
+else:
+    print("not valied number")   
+           
