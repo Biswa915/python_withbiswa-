88ca5db6bada317f8f9c2f7c82233c2ef8c0979b
Biswajit <bs5473287@gmail.com>
2026-08-10 13:23:27 +0530

    added folder

---
+++ comparisiono operators.py
@@ -0,0 +1,10 @@
+#comparision operators
+a = 10
+b = 5
+ 
+print(a == b)  # False
+print(a != b)  # True
+print(a < b)   # False
+print(a > b)   # True
+print(a <= b)  # False
+print(a >= b)  # True
