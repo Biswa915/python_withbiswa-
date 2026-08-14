f8d207b2c33e87fc927118bb2f86287f2bd33e36
Biswajit <bs5473287@gmail.com>
2026-08-15 02:32:06 +0530

    added new file

--- python_guru/while_loop question.py
+++ python_guru/while_loop question.py
@@ -8,3 +8,25 @@
 # Q18. Ask a number from the user, print the multiplication table upto 10
 
 # Q19. Ask a number from the user, and print all the factors.
+
+#Q1
+# start=int(input("enter your number="))
+# end=int(input("enter your number="))
+
+# i=start
+# while i <=end:
+#     if i %3 ==0 and i %5==0:
+#          print(i, end=" ")
+#     i +=1
+
+#q2
+start=int(input("enter your number="))
+end=int(input("enter your number="))
+
+i=start
+total = 0
+while i<=end:
+     total= total+i
+     i+=1
+print(f"total = {total}")
+     
