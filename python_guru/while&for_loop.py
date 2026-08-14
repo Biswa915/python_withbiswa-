19295a43582af8df7db0a97d8cefac8868655044
Biswajit <bs5473287@gmail.com>
2026-08-14 23:47:38 +0530

    added new file

--- python_guru/while&for_loop.py
+++ python_guru/while&for_loop.py
@@ -3,15 +3,26 @@
 """"""""
 
 # while condition:
-#print 1to10 
-i=1
-while i<=10:
-    print(i,end=' ')
-    i+=1
+#1 print 1to10 
+# i=1
+# while i<=10:
+#     print(i,end=' ')
+#     i+=1
 
- #1 to n print,n is the number input by user
-n=int(input('enter your namber='))
-i=1
-while i <= n:
+ #2  1 to n print,n is the number input by user
+# n=int(input('enter your namber='))
+# i=1
+# while i <= n:
+#     print(i, end=" ")
+#     i +=1
+
+#3 strat to end user using while loop
+strat =int(input("enter your number=")) #1
+end =int(input("enter your number=")) #20
+i=strat
+while i <=end:
     print(i, end=" ")
     i +=1
+
+
+    print(f"After while loop, start value is {start}")
