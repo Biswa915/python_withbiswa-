54025740111125bc868ffe61a4db71dd390596ef
Biswajit <bs5473287@gmail.com>
2026-08-15 01:36:38 +0530

    added new file

--- python_guru/while&for_loop.py
+++ python_guru/while&for_loop.py
@@ -29,10 +29,20 @@
 
 #4  start to end print even numbers
 
+# start = int(input("Enter start number ="))
+# end = int(input("Enter end number =")) 
+# i= start
+# while i <= end:
+#     if i %2 ==0:
+#       print(i, end=" ")  
+#     i +=1
+
+#5 # print start to end, numbers which are divisble by 3 and 4
+
 start = int(input("Enter start number ="))
-end = int(input("Enter end number =")) # 11
-i= start
-while i <= end:
-    if i /2 ==0:
-      print(i, end=" ")  
+end = int(input("Enter end number ="))
+i=start
+while i<= end:
+    if (i %3==0 or  i %4==0) and i %5==0:
+        print(i, end=" ")
     i +=1
