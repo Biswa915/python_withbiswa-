2547850c3faf1330efe7f96f0837ceca0bc06827
Biswajit <bs5473287@gmail.com>
2026-08-15 01:43:06 +0530

    added new file

--- python_guru/while&for_loop.py
+++ python_guru/while&for_loop.py
@@ -39,10 +39,12 @@
 
 #5 # print start to end, numbers which are divisble by 3 and 4
 
-start = int(input("Enter start number ="))
-end = int(input("Enter end number ="))
-i=start
-while i<= end:
-    if (i %3==0 or  i %4==0) and i %5==0:
-        print(i, end=" ")
-    i +=1
+# start = int(input("Enter start number ="))
+# end = int(input("Enter end number ="))
+# i=start
+# while i<= end:
+#     if (i %3==0 or  i %4==0) and i %5==0:
+#         print(i, end=" ")
+#     i +=1
+
+#6  
---
+++ python_guru/while_loop question.py
@@ -0,0 +1,10 @@
+# Practice Questions on WHILE Loop
+# Q15. Print all the numbers which are divisible by 3 and 5, from 1 to 100.
+
+# Q16. Sum of all the numbers from 1 to 100.
+
+# Q17. Sum of all the numbers from 1 to 100 divisible by 2 and 7.
+
+# Q18. Ask a number from the user, print the multiplication table upto 10
+
+# Q19. Ask a number from the user, and print all the factors.
