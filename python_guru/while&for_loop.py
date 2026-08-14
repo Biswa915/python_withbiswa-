c60a4c29b375a53143330fd66ba16580dff5d8b0
Biswajit <bs5473287@gmail.com>
2026-08-14 23:20:35 +0530

    added new folder

---
+++ python_guru/while&for_loop.py
@@ -0,0 +1,17 @@
+# A loop lets you run the same block of code multiple times without writing
+# it again and again. Instead of doing this:
+""""""""
+
+# while condition:
+#print 1to10 
+i=1
+while i<=10:
+    print(i,end=' ')
+    i+=1
+
+ #1 to n print,n is the number input by user
+n=int(input('enter your namber='))
+i=1
+while i <= n:
+    print(i, end=" ")
+    i +=1
