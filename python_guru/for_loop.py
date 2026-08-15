1f8f7200876da43b7e13c7a6dac8e6669a649638
Biswajit <bs5473287@gmail.com>
2026-08-15 12:45:56 +0530

    added new file

---
+++ python_guru/for_loop.py
@@ -0,0 +1,16 @@
+# FOR loop
+# A for loop is used to iterate over a sequence - a range of numbers, a string, a list,
+# and so on. Unlike while, you don't manage a counter manually - Python
+# handles it for you.
+
+# for variable in sequence:
+# this block runs for each item in the sequence
+
+# Looping over a range of numbers
+# for i in range(1, 6):
+# print (i)
+# Output: 1 2 3 4 5
+
+# print 1 to 10
+for i in range(1, 12):
+    print(i,end=" ") 
