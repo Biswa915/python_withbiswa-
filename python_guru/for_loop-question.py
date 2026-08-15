d463f9c037dfe1e53b2ee2329747d0c145d88f3c
Biswajit <bs5473287@gmail.com>
2026-08-15 23:48:59 +0530

    added new file

---
+++ python_guru/for_loop-question.py
@@ -0,0 +1,31 @@
+# Practice Questions on for Loop
+# # Q15. Print all the numbers which are divisible by 3 and 5, from 1 to 100.
+
+# # Q16. Sum of all the numbers from 1 to 100.
+
+# # Q17. Sum of all the numbers from 1 to 100 divisible by 2 and 7.
+
+# # Q18. Ask a number from the user, print the multiplication table upto 10
+
+# # Q19. Ask a number from the user, and print all the factors.
+
+# Q1
+# start = int(input("Enter start number = ")) 
+# end=int(input("Enter end number = ")) 
+
+# count=0
+# for i in range (start,end):
+#     if i %3 ==0 and i %5==0:
+#         print(i,end=" ")
+#         count +=1
+# print(f" count of number division {count}")
+
+# q2
+start=int(input("enter your number="))
+end=int(input("enter your number="))
+
+total=0
+for i in range(start,end+1):
+    total +=i
+print(f"sum ={total}")
+
