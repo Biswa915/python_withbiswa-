61d9e7de3455338278b5c8ed39efc68eeb579e10
Biswajit <bs5473287@gmail.com>
2026-08-15 12:56:45 +0530

    added new file

--- python_guru/for_loop.py
+++ python_guru/for_loop.py
@@ -16,6 +16,10 @@
 #     print(i,end=" ") 
 
 #step in for loop
-# for i in range(1, 12,4):
-#     print(i,end=" ") 
+for i in range(1, 12,4):
+     print(i,end=" ") 
     
+# loop revers and divisible cheek
+for i in range(100,0,-1):
+    if i % 2==0 and i %3==0:
+        print(i, end=" ")
