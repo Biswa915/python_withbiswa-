513a96ec49842f8385ac29534c6523118d30d66d
Biswajit <bs5473287@gmail.com>
2026-08-15 13:40:10 +0530

    added new file

---
+++ while_for-loop.py/while&_loop.py
@@ -0,0 +1,50 @@
+# A loop lets you run the same block of code multiple times without writing
+# it again and again. Instead of doing this:
+""""""""
+
+# while condition:
+#1 print 1to10 
+# i=1
+# while i<=10:
+#     print(i,end=' ')
+#     i+=1
+
+ #2  1 to n print,n is the number input by user
+# n=int(input('enter your namber='))
+# i=1
+# while i <= n:
+#     print(i, end=" ")
+#     i +=1
+
+#3 strat to end user using while loop
+# start =int(input("enter your number=")) #1
+# end =int(input("enter your number=")) #20
+# i=start
+
+# while i <= end:
+#     print(i, end=" ")
+#     i += 1
+
+# print(f"After while loop, start value is {start}")
+
+#4  start to end print even numbers
+
+# start = int(input("Enter start number ="))
+# end = int(input("Enter end number =")) 
+# i= start
+# while i <= end:
+#     if i %2 ==0:
+#       print(i, end=" ")  
+#     i +=1
+
+#5 # print start to end, numbers which are divisble by 3 and 4
+
+# start = int(input("Enter start number ="))
+# end = int(input("Enter end number ="))
+# i=start
+# while i<= end:
+#     if (i %3==0 or  i %4==0) and i %5==0:
+#         print(i, end=" ")
+#     i +=1
+
+#6  
---
+++ while_for-loop.py/while_loop question.py
@@ -0,0 +1,68 @@
+# Practice Questions on WHILE Loop
+# Q15. Print all the numbers which are divisible by 3 and 5, from 1 to 100.
+
+# Q16. Sum of all the numbers from 1 to 100.
+
+# Q17. Sum of all the numbers from 1 to 100 divisible by 2 and 7.
+
+# Q18. Ask a number from the user, print the multiplication table upto 10
+
+# Q19. Ask a number from the user, and print all the factors.
+
+#Q15
+# start=int(input("enter your number="))
+# end=int(input("enter your number="))
+
+# i=start
+# while i <=end:
+#     if i %3 ==0 and i %5==0:
+#          print(i, end=" ")
+#     i +=1
+
+#q16
+# start=int(input("enter your number="))
+# end=int(input("enter your number="))
+
+# i=start
+# total = 0
+# while i<=end:
+#      total= total+i
+#      i+=1
+
+# print(f"total = {total}")
+
+#q17
+# start=int(input("enter your number="))
+# end=int(input("enter your number="))
+
+# i=start
+# count = 0 # total=0
+# while i<=end:
+#      if i % 2==0 and i%7==0:
+#        print(i)
+#        count= count+1 #/ total= total+i
+#      i+=1
+     
+# print(f"total count 2 and 7 = {count}") # print(f"total{total}"")
+
+# Q18  :
+# num=int(input("enter your number="))
+# i=1
+# while i<=10:
+#     div= num*i
+#     print(f"{num}x{i}={div}")
+#     i +=1
+
+# Q19 
+# num=int(input('enter your num='))
+# i=1
+# count=0
+# while i<=num:
+#     if num %i==0:
+#      count=count+1
+#     i+=1
+# print(f'total factor number{num} and counting number{count}')
+
+
+
+     
