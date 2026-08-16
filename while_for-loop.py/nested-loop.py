015475be5665c1e5e90b093fa2b3570703aa808c
Biswajit <bs5473287@gmail.com>
2026-08-16 21:28:12 +0530

    added new file

--- while_for-loop.py/nested-loop.py
+++ while_for-loop.py/nested-loop.py
@@ -2,13 +2,28 @@
 # A nested loop is a loop inside another loop. The inner loop completes all its
 # iterations for every single iteration of the outer loop.
 # q1
-# 1 2 3 4 5 
-# 1 2 3 4 5 
-# 1 2 3 4 5 
-# 1 2 3 4 5 
-# 1 2 3 4 5 
+'''''
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5
+''''''''' 
 
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print()
+
+# q2
+"""""
+1 1 1 1 1 
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+"""""
 for i in range(1,6):
     for j in range(1,6):
-        print(j,end=" ")
+        print(i, end=" ")
     print()
