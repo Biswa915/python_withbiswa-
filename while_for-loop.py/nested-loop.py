f32aeeb95151384384361812a7c86b5878233d0d
Biswajit <bs5473287@gmail.com>
2026-08-18 21:47:22 +0530

    Add new css and new html

--- while_for-loop.py/nested-loop.py
+++ while_for-loop.py/nested-loop.py
@@ -102,9 +102,21 @@
 7 6
 7
 '''
-n=int(input("enter your number="))
-for i in range(1,n+1):
-    for j in range(n,i-1,-1):
-        print(j, end=" ")
+# n=int(input("enter your number="))
+# for i in range(1,n+1):
+#     for j in range(n,i-1,-1):
+#         print(j, end=" ")
+#     print()    
+
+# q9
+'''''
+5 4 3 2 1
+4 3 2 1 
+3 2 1 
+2 1 
+1 
+'''
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=' ')
     print()    
-   
