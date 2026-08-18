4b0d79533bbd78206e8ca9552d5d5d86d273334c
Biswajit <bs5473287@gmail.com>
2026-08-18 21:10:11 +0530

    Add new css and new html

--- while_for-loop.py/nested-loop.py
+++ while_for-loop.py/nested-loop.py
@@ -68,7 +68,14 @@
 #     print() 
 
 # q6
+'''''
+1 2 3 4 5
+1 2 3 4 
+1 2 3 
+1 2
+1 
+'''
 for i in range(6,0,-1):
     for j in range(1,i+1):
-        print("*",end=" ")
+        print(j,end=" ")
     print()
