5dca87973e9b036cc79eb58096ea8a612d2d0bc1
Biswajit <bs5473287@gmail.com>
2026-08-21 02:29:56 +0530

    Add new css and new html

--- python_guru/function.py/parameters&argument.py
+++ python_guru/function.py/parameters&argument.py
@@ -9,13 +9,13 @@
 
 
 # parameter 3 int as a ,print thhe total
-def addition(a,b,c):
-   print(f"name is {a} and age {b} gender is {c}")
-n=input("enter your name=")
-a=int(input("enter your name="))
-g=input("enter your gender=")
-addition(n,a,g)   
+# def addition(a,b,c):
+#    print(f"name is {a} and age {b} gender is {c}")
+# n=input("enter your name=")
+# a=int(input("enter your name="))
+# g=input("enter your gender=")
+# addition(n,a,g)   
 
 def greet(n1,n2):
    print(f"total={n1+n2}")
-greet()
+greet(20,40)
