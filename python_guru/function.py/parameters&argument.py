6e613d1d90cc292b9f58b9c210a5bd0797cbc0be
Biswajit <bs5473287@gmail.com>
2026-08-21 02:26:10 +0530

    Add new css and new html

--- python_guru/function.py/parameters&argument.py
+++ python_guru/function.py/parameters&argument.py
@@ -3,6 +3,15 @@
 # is the actual value you pass when calling the function. Parameters make
 # functions flexible - the same function can work with different data every time.
 # example: 
-def greet(name):
-    print(f"hello {name}!")
-greet("biswa")    
+# def greet(name):
+#     print(f"hello {name}!")
+# greet("biswa")    
+
+
+# parameter 3 int as a ,print thhe total
+def addition(a,b,c):
+   print(f"name is {a} and age {b} gender is {c}")
+n=input("enter your name=")
+a=int(input("enter your name="))
+g=input("enter your gender=")
+addition(n,a,g)   
