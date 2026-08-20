06b4afb60eb32a3169beba127b04777843b8d62f
Biswajit <bs5473287@gmail.com>
2026-08-21 02:06:49 +0530

    Add new css and new html

---
+++ python_guru/function.py/parameters&argument.py
@@ -0,0 +1,4 @@
+# Parameters and Arguments
+# A parameter is a variable listed inside the function definition. An argument
+# is the actual value you pass when calling the function. Parameters make
+# functions flexible - the same function can work with different data every time.
