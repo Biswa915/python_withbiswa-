8d09fa7c7d7ae9d84e4c3b25598d0e9f75ae5980
Biswajit <bs5473287@gmail.com>
2026-08-21 02:09:35 +0530

    Add new css and new html

--- python_guru/function.py/parameters&argument.py
+++ python_guru/function.py/parameters&argument.py
@@ -2,3 +2,7 @@
 # A parameter is a variable listed inside the function definition. An argument
 # is the actual value you pass when calling the function. Parameters make
 # functions flexible - the same function can work with different data every time.
+# example: 
+def greet(name):
+    print(f"hello {name}!")
+greet("biswa")    
