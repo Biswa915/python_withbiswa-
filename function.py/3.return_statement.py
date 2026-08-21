00a17027c6414fd5c7b09a81aef064cd045128c4
Biswajit <bs5473287@gmail.com>
2026-08-21 22:31:06 +0530

    Add new css and new html

---
+++ function.py/3.return_statement.py
@@ -0,0 +1,4 @@
+# return Statement
+# The return statement sends a value back to whoever called the function.
+# Without return, a function just does something but gives nothing back.
+# With return, you can use the result in the rest of your program.
