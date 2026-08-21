ea3bb8b46407ab7011ea67cbbe44c0f38b3e4861
Biswajit <bs5473287@gmail.com>
2026-08-21 15:04:47 +0530

    Add new css and new html

---
+++ function.py/questionfor-function.py
@@ -0,0 +1,22 @@
+# Practice Questions on Parameters and Arguments
+
+# Q37. Write a function called add that takes two numbers as parameters
+# and prints their sum.
+
+# Q38. Write a function called rectangle_area that takes length and breadth
+# as parameters and prints the area.
+
+# Q39. Write a function called find max that takes three numbers as
+# parameters and prints the largest one.
+
+# Q40. Write a function called discount_price that takes original_price
+# and discount_percent as parameters and prints the final
+# price after discount ..
+
+
+# q1
+def addition(a1,b1):
+    ans=a1+b1
+    print(f'total={ans}')
+
+addition(123,43)    
