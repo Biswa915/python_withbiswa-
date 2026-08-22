3846468e7b61b74b6049d8b515709572ceeef3b6
Biswajit <bs5473287@gmail.com>
2026-08-22 11:28:07 +0530

    Add new css and new html

--- function.py/3.return_statement.py
+++ function.py/3.return_statement.py
@@ -4,8 +4,17 @@
 # With return, you can use the result in the rest of your program.
 
 # example-
-def num(n1,n2,n3):
-    return n1+n2+n3
+# def num(n1,n2,n3):
+#     return n1+n2+n3
 
-ans=num(10,50,32)
+# ans=num(10,50,32)
+# print(ans)
+
+# true and false return statement
+def can_voter(age):
+    if age>18:
+        return True
+    return False
+
+ans=can_voter(17)
 print(ans)
