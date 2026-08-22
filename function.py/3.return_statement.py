3455e589888174d7ea97f214faf641d3e76ee100
Biswajit <bs5473287@gmail.com>
2026-08-22 11:19:03 +0530

    Add new css and new html

--- function.py/3.return_statement.py
+++ function.py/3.return_statement.py
@@ -2,3 +2,10 @@
 # The return statement sends a value back to whoever called the function.
 # Without return, a function just does something but gives nothing back.
 # With return, you can use the result in the rest of your program.
+
+# example-
+def num(n1,n2,n3):
+    return n1+n2+n3
+
+ans=num(10,50,32)
+print(ans)
