2a0972a486a8740767919be4ff46349268d1d903
Biswajit <bs5473287@gmail.com>
2026-08-25 22:49:38 +0530

    added new file

--- pratice-question.py/oprater.py
+++ pratice-question.py/oprater.py
@@ -3,8 +3,14 @@
 # print(a+b,a-b,a%b,a*b)
 
 
-def biswa(a,b):
-    
-    return f"{a+b},{a-b},{a*b},{a%b}"
+# def biswa(a,b):
 
-print(biswa(10,20))
+#     return f"{a+b},{a-b},{a*b},{a%b}"
+
+# print(biswa(10,20))
+
+# Take a number as input. Print whether it is even or odd using the %
+# operator and a comparison operator
+a=int(input("enter your number="))
+c=a%2==0
+print(c)
