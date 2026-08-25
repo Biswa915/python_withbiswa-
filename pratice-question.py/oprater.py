cba6fd04aefa087d2e29dfaa9a0bd9d25efb530e
Biswajit <bs5473287@gmail.com>
2026-08-25 22:34:27 +0530

    added new file

--- pratice-question.py/oprater.py
+++ pratice-question.py/oprater.py
@@ -1,3 +1,10 @@
-a=int(input("enter your number="))
-b=int(input("enter your number="))
-print(a+b,a-b,a%b,a*b)
+# a=int(input("enter your number="))
+# b=int(input("enter your number="))
+# print(a+b,a-b,a%b,a*b)
+
+
+def biswa(a,b):
+    
+    return f"{a+b},{a-b},{a*b},{a%b}"
+
+print(biswa(10,20))
