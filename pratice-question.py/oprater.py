eaa7c932fbc2eb391a75097a7cdfc7ae36e3ae55
Biswajit <bs5473287@gmail.com>
2026-08-29 01:52:47 +0530

    added new file

--- pratice-question.py/oprater.py
+++ pratice-question.py/oprater.py
@@ -207,3 +207,27 @@
 #     if num % i==0:
 #         print(i,end=" ")
 #     i+=1
+
+# Q20. Take numbers as input from the user one by one. Skip negative
+# numbers and keep adding the positive ones. Stop when the user
+# enters 0 and print the total. (Uses both continue and break.)
+# num=int(input("enter a number ="))
+# total=0
+# i=True
+# for i in range(num-1):
+#     if num==0:
+#         break
+#     if num<0:
+#         continue
+#     total=total+1
+# print(total)
+total=0
+while True:
+    num=int(input('enter your number='))
+    if num == 0:
+        break
+    if num <0 :
+        continue
+    total += num
+    
+print(total)
