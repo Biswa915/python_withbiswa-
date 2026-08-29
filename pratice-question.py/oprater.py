5340c8780a45216e3ebd0199fde867e627fb072d
Biswajit <bs5473287@gmail.com>
2026-08-29 17:27:11 +0530

    added new file

--- pratice-question.py/oprater.py
+++ pratice-question.py/oprater.py
@@ -392,15 +392,31 @@
            1
 
 """
-for i in range(1,6):
-    for j in range(1,5-i +1):
-        print(" ",end=" ")
-    for k in range(1,(i*2) -1+1):
-         print(k,end=" ")
-    print()
-for i in range(4,0,-1):
-    for j in range(1,5-i +1):
-        print(" ",end=" ")
-    for k in range(1,(i*2)-1+1):
-         print(k,end=" ")
+# for i in range(1,6):
+#     for j in range(1,5-i +1):
+#         print(" ",end=" ")
+#     for k in range(1,(i*2) -1+1):
+#          print(k,end=" ")
+#     print()
+# for i in range(4,0,-1):
+#     for j in range(1,5-i +1):
+#         print(" ",end=" ")
+#     for k in range(1,(i*2)-1+1):
+#          print(k,end=" ")
+#     print()
+
+# Q30
+''''
+1
+2 3
+4 5 6 
+7 8 9 10
+11 12 13 14 15 
+'''
+num=int(input("enter a number ="))
+total=1
+for i in range(1,num+1):
+    for j in range(i):
+       print(total,end=" ")
+       total+=1
     print()
