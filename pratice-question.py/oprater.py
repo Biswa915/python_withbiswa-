2452c1f5e670086f5ca9e547cdd0aff7450f7dca
Biswajit <bs5473287@gmail.com>
2026-08-29 18:07:45 +0530

    added new file

--- pratice-question.py/oprater.py
+++ pratice-question.py/oprater.py
@@ -443,10 +443,25 @@
 0 1 0 1 0 
 1 0 1 0 1 
 '''
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if(i+j)%2==0:
+#           print(1,end=" ")
+#         else:
+#             print(0,end=" ")
+#     print()
+# q34
+''''
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+'''
 for i in range(1,6):
     for j in range(1,6):
-        if(i+j)%2==0:
-          print(1,end=" ")
+        if i == 1 or i == 5 or j == 1 or j == 5:
+         print("*",end=" ")
         else:
-            print(0,end=" ")
+           print(" ",end=" ")
     print()
