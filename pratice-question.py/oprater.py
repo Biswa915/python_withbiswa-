bb2a8977348406b05845b1538bfd20d09ff9ec2c
Biswajit <bs5473287@gmail.com>
2026-08-29 14:58:23 +0530

    added new file

--- pratice-question.py/oprater.py
+++ pratice-question.py/oprater.py
@@ -318,7 +318,42 @@
 5
 1
 '''
-for i in range(5,0,-1):
-     for j in range(i,0,-1):
-          print(j,end=" ")
-     print()
+# for i in range(5,0,-1):
+#      for j in range(i,0,-1):
+#           print(j,end=" ")
+#      print()
+# q27
+''''
+7 6 5 4 3 2 1 
+7 6 5 4 3 2 
+7 6 5 4 3
+7 6 5 4
+7 6 5
+7 6
+7
+'''
+# for i in range(1,8):
+#     for j in range(7,i-1,-1):
+#         print(j,end=" ")
+#     print()
+
+# q28
+''''
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+1 2 3 4
+1 2 3
+1 2
+1
+'''
+for i in range (1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+for i in range(4,0,-1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
