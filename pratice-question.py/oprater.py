9cc48d12dfe5913e65e037ff63ace3a7b4e399c0
Biswajit <bs5473287@gmail.com>
2026-08-30 15:46:15 +0530

    added new file

--- pratice-question.py/oprater.py
+++ pratice-question.py/oprater.py
@@ -458,10 +458,19 @@
 *       *
 * * * * *
 '''
-for i in range(1,6):
-    for j in range(1,6):
-        if i == 1 or i == 5 or j == 1 or j == 5:
-         print("*",end=" ")
-        else:
-           print(" ",end=" ")
-    print()
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i == 1 or i == 5 or j == 1 or j == 5:
+#          print("*",end=" ")
+#         else:
+#            print(" ",end=" ")
+#     print()
+
+# Q37. Write a function called add that takes two numbers as parameters
+# and prints their sum.
+def biswa(a,b):
+    ans=a+b
+    return ans
+
+a=biswa(10,29) 
+print(a)
