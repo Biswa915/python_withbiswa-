ae2fc96ce16e0b6da9e2328055b53afb4b6e296f
Biswajit <bs5473287@gmail.com>
2026-09-02 02:24:30 +0530

    Add new css and new html

--- function.py/LocalvsGlobal.py
+++ function.py/LocalvsGlobal.py
@@ -2,3 +2,4 @@
 # Scope refers to where a variable is accessible in your code. A variable created
 # inside a function is local it only exists inside that function. A variable created
 # outside all functions is global it can be accessed from anywhere.
+def 
