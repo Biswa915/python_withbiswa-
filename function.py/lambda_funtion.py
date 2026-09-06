85746e5758882f7b31cf76f333aaaa689805f65d
Biswajit <bs5473287@gmail.com>
2026-09-06 21:33:10 +0530

    Add new file

--- function.py/lambda_funtion.py
+++ function.py/lambda_funtion.py
@@ -2,6 +2,7 @@
 # A lambda function is a small anonymous function written in a single line. lt
 # is useful when you need a simple function for a short period and do
 # not want to formally define it with def.
-square= lambda n:n+n
+square = lambda n: n+n
 
 print(square(10))
+print(square(20))
