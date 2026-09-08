d6917ecd9604f7a13d04cc4e3d960e920cfcad80
Biswajit <bs5473287@gmail.com>
2026-09-09 02:04:20 +0530

    Update Python files

--- List_in_python/crating_list.py
+++ List_in_python/crating_list.py
@@ -1,4 +1,17 @@
 # crating a list
-x=["biswa","moti","guru"]
-print(x)
-print(type(x))
+# x=["biswa","moti","guru"]
+# print(x)
+# print(type(x))
+
+# oparation in list 
+# marks=[8,5,7,5,]
+# print(marks*5)
+# marks=[8,5,7,5,]
+# marks1=[1,8,5,7,5,]
+
+# print(marks+marks1)
+
+# bulit in function
+# len()get length
+mark=[2,6,7,56,7,5]
+print(len(mark))
