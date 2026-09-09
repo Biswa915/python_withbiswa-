9c6d139e88a396cab9ebdcaccfce4f9bbbf69324
Biswajit <bs5473287@gmail.com>
2026-09-10 01:56:17 +0530

    Update Python files

---
+++ List_in_python/list_question.py
@@ -0,0 +1,6 @@
+# Question 49: "
+# Favourite Movies Create a list of 5 of your favourite movies. Print the first, last,
+# and middle movie from your list using both positive and negative indexing where appropriate.
+lis=["danus","monye","game of throne","stanger think","majo bou"]
+n=len(lis)
+print(f"find the movie fast = {lis[n-5 ]} last = {lis[n-1]} and midddle ={lis[n-3]}")
