86c1d5cec32f4d81138635cd59e6faf9e7b9b3c9
Biswajit <bs5473287@gmail.com>
2026-09-11 02:04:12 +0530

    Update Python files

--- List_in_python/iteration using whlie loop.py
+++ List_in_python/iteration using whlie loop.py
@@ -1,6 +1,23 @@
 num=[10,25,33,9,17,20,71,42,55,24,27]
 n=len(num)
-i=0
-while i <=n-1:
-    print(num[i], end=" ")
-    i +=1
+# i=0
+# while i <=n-1:
+#     print(num[i], end=" ")
+#     i +=1
+
+# count=0
+# while i <=n-1:
+#     if num [i] %2==0:
+#         count +=1
+#     i+=1
+# print(count)
+
+# i=n-1
+# while i>=0:
+#     print(num[i],end=" ")
+#     i-=1
+
+total=0
+for i in range(0,n):  
+     total= total + num[i]
+print(total)
