ec6e257e456563333da340e9c0349df2f8cb2f3d
Biswajit <bs5473287@gmail.com>
2026-09-10 21:35:37 +0530

    Update Python files

--- List_in_python/list_question.py
+++ List_in_python/list_question.py
@@ -3,7 +3,10 @@
 # and middle movie from your list using both positive and negative indexing where appropriate.
 lis=["danus","monye","game of throne","stanger think","majo bou"]
 n=len(lis)
-print(f"find the movie fast = {lis[n-5 ]} last = {lis[n-1]} and midddle ={lis[n-3]}")
+print(f"fast movie = {lis[n-5]}")
+print(f"last movie = {lis[n-1]}")
+print(f" middel move = {lis[n//2]}")
+    
 
 
 # Question 50: 
