689096709a559cfc1d08af86cfa24c9d21f2bafe
Biswajit <bs5473287@gmail.com>
2026-09-21 23:19:34 +0530

    Update Python files

--- List_in_python/enumerate.py
+++ List_in_python/enumerate.py
@@ -2,4 +2,5 @@ num=[10,25,33,9,17,20,71,42,55,24,27]
 
 
 for index , value in enumerate(num):
-    print(f"index = {index} and value = {value}")
+    if value %2==0:
+     print(f"index = {index} and value = {value}")
